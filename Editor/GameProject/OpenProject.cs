b362f08281e9e69cf8aaaea7ab30bd3d7574c022
why <498152246@qq.com>
2024-03-18 18:32:38 +0800

    [Add] OpenProject Related.

--- Editor/GameProject/OpenProject.cs
+++ Editor/GameProject/OpenProject.cs
@@ -1,9 +1,32 @@
-﻿using Editor.Common;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
 
 namespace Editor.GameProject
 {
+    [DataContract]
+    public class ProjectData
+    {
+        [DataMember]
+        public string ProjectName { get; set; }
+        [DataMember]
+        public string ProjectPath { get; set; }
+        [DataMember]
+        public DateTime Data { get; set; }
+    }
+
+    [DataContract]
+    public class ProjectDataList
+    {
+        [DataMember]
+        public List<ProjectData> Projects { get; set; }
+    }
     public class OpenProject
     {
-        
+        static OpenProject()
+        {
+            
+        }
     }
 }
