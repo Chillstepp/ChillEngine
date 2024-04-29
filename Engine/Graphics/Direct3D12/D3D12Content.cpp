7a35e073b61603d8042f379b9b16696537dcde35
why <498152246@qq.com>
2024-04-29 14:14:28 +0800

    [Fix] 1.Remove useless variables  2.low-level render item move to cpp for the reason that it won't be used by GameDev.

--- Engine/Graphics/Direct3D12/D3D12Content.cpp
+++ Engine/Graphics/Direct3D12/D3D12Content.cpp
@@ -12,6 +12,17 @@ namespace ChillEngine::graphics::d3d12::content
             id::id_type gpass_pso_id = id::invalid_id;
             id::id_type depth_pso_id = id::invalid_id;
         };
+
+        //all we need to render a submesh
+        struct d3d12_render_item
+        {
+            id::id_type entity_id;
+            id::id_type submesh_gpu_id;
+            id::id_type material_id;
+            id::id_type pso_id;
+            id::id_type depth_pso_id;
+        };
+        
         struct submesh_view
         {
             D3D12_VERTEX_BUFFER_VIEW        position_buffer_view{};
@@ -33,7 +44,7 @@ namespace ChillEngine::graphics::d3d12::content
         utl::free_list<std::unique_ptr<u8[]>>   materials;
         std::mutex                              material_mutex{};
 
-        utl::free_list<render_item::d3d12_render_item>  render_items;
+        utl::free_list<d3d12_render_item>  render_items;
         utl::free_list<std::unique_ptr<id::id_type[]>>  render_item_ids;
         utl::vector<ID3D12PipelineState*>               pipeline_states;
         std::unordered_map<u64, id::id_type>            pso_map;
@@ -42,7 +53,6 @@ namespace ChillEngine::graphics::d3d12::content
         struct {
             utl::vector<ChillEngine::content::lod_offset>    lod_offsets;
             utl::vector<id::id_type>                    geometry_ids;
-            utl::vector<f32>                            thresholds;
         } frame_cache;
         
         id::id_type create_root_signature(material_type::type type, shader_flags::flags flags);
@@ -531,7 +541,7 @@ namespace ChillEngine::graphics::d3d12::content
     namespace render_item 
     {
         //buffer[0] = geometry_content_id
-        //buffer[1 .. n] = d3d12_render_item_ids
+        //buffer[1 ... n] = d3d12_render_item_ids(n is the number of love-level render item ids ,it is also equal to the number of submeshes/materials ids)
         //buffer[n+1] = invalid_id
         //Add a render item(render item contains a geometry and several of materials)
         id::id_type add(id::id_type entity_id, id::id_type geometry_content_id, u32 material_count, const id::id_type *const material_ids)
@@ -602,7 +612,6 @@ namespace ChillEngine::graphics::d3d12::content
 
             frame_cache.lod_offsets.clear();
             frame_cache.geometry_ids.clear();
-            frame_cache.thresholds.clear();
             const u32 count{ info.render_item_count };
 
             std::lock_guard lock{ render_item_mutex };
@@ -611,10 +620,9 @@ namespace ChillEngine::graphics::d3d12::content
             {
                 const id::id_type *const buffer{ render_item_ids[info.render_item_ids[i]].get() };
                 frame_cache.geometry_ids.emplace_back(buffer[0]);
-                frame_cache.thresholds.emplace_back(info.thresholds[i]);
             }
 
-            ChillEngine::content::get_lod_offset(frame_cache.geometry_ids.data(), frame_cache.thresholds.data(), count, frame_cache.lod_offsets);
+            ChillEngine::content::get_lod_offset(frame_cache.geometry_ids.data(), info.thresholds, count, frame_cache.lod_offsets);
             assert(frame_cache.lod_offsets.size() == count);
 
             u32 d3d12_render_item_count{ 0 };
